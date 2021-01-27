a2e289948e1a208e27f7e5b9d9fcf8de61751ce9
Nikita-Thinkitive <77092799+Nikita-Thinkitive@users.noreply.github.com>
2021-01-27 20:38:55 +0530

    Add files via upload

---
+++ src/com/thinkitive/Register/Demo.java
@@ -0,0 +1,11 @@
+package com.thinkitive.Register;
+
+public class Demo {
+	public static void main(String[] args) {
+		DbOperation op=new DbOperation();
+		//op.create();
+		op.insertEmp("aa", "b");
+		//op.search("aa", "b");
+	}
+
+}
